9bf1c58580936afdd170217039859b23ab269500
Lyle Arcinas <60361950+lel-rc@users.noreply.github.com>
2025-10-14 14:09:24 -0500

    Started adding binding

--- server.py
+++ server.py
@@ -2,13 +2,28 @@ import select
 import sys
 import socket
 
+hostname = socket.gethostname()
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def main():
-    getArgs()
+    bindSockets()
 
-def getArgs():
+def bindSockets():
+    try:
+        verifyArgs()
+    except ValueError as e:
+        print(e)
+
+    try:
+        clientsocket.bind((hostname, sys.argv[1]))
+    except OSError as e:
+        print(e)
+
+
+def verifyArgs():
     if len(sys.argv) != 3:
         raise ValueError("Arguments must be in the form <clientport> <workerport>")
+
 
 if __name__=="__main__":
     main()
