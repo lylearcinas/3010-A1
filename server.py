343d252d9238ce385dae6989cdc99bafe21115a0
Lyle Arcinas <60361950+lel-rc@users.noreply.github.com>
2025-10-14 23:06:30 -0500

    Added job numbers

--- server.py
+++ server.py
@@ -12,36 +12,29 @@ def main():
 def runProgram():
     try:
         verifyArgs()
-        try:
-            clientsocket.bind((hostname, int(sys.argv[1])))
-            clientsocket.listen(5)
+        clientsocket.bind((hostname, int(sys.argv[1])))
+        clientsocket.listen(5)
 
-            while True:
-                conn,addr = clientsocket.accept()
-                with conn:
-                    try:
-                        print('Connected by', addr)
-                        data = conn.recv(1024)
-                        dataText = data.decode('UTF-8').strip()
-                        print("heard:")
-                        print(dataText)
-                        queue.append(dataText) 
-                        conn.sendall(b"Received ")
-                        print("Array is now: " + str(queue))
-                    except Exception as e:
-                        print(e)
-        except OSError as e:
-            print(e)
-    except ValueError as e:
+        while True:
+            conn,addr = clientsocket.accept()
+
+            with conn:
+                print('Connected by', addr)
+                data = conn.recv(1024)
+                dataText = data.decode('UTF-8').strip()
+                print("heard:")
+                print(dataText)
+                queue.append(dataText) 
+                receivedText = "Received JOB with ID <{}>".format(str(len(queue)))
+                receivedText = bytes(receivedText, encoding='utf8')
+                conn.sendall(receivedText)
+                print("Array is now: " + str(queue))
+    except Exception as e:
         print(e)
-    
-    
-
 
 def verifyArgs():
-    if (sys.argv):
-        if len(sys.argv) != 3:
-            raise ValueError("Arguments must be in the form <clientport> <workerport>")
+    if len(sys.argv) != 3:
+        raise ValueError("Arguments must be in the form <clientport> <workerport>")
 
 
 if __name__=="__main__":
